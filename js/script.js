c9c60d5b35982f5692bff06a01abd58621fe3b56
Kellin Rieper <>
2023-01-27 00:30:18 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.getElementById("botaoenviar").addEventListener("click", validaFormulario)
+function validaFormulario() {
+	if(document.getElementById("nome").value !=""  && 						 	document.getElementById("email").value !="" && document.getElementById("telefone").value !=""){
+	alert( "Prontinho! Você receberá as novidades por emai!")
+	}else{
+		alert( "Por favor, preencha os campos nome, email e telefone!")
+
+	}
+
+}
+
+
+
+
+
+
